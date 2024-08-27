224772015ab67682c42db7c282955d484a02e7e8
Abdul Wahith <48100842+iwahith@users.noreply.github.com>
2024-08-27 17:10:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import requests
+import json
 
 app = FastAPI()
 
+receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt'
+item_list = []
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/upload/")
+async def create_upload_file(uploaded_file: UploadFile = File(...)):
+    file_location = f"data/{uploaded_file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(await uploaded_file.read())
+
+    with open(file_location, "rb") as file_object:
+        r = requests.post(receiptOcrEndpoint, data={
+            'client_id': 'TEST',
+            'recognizer': 'auto',
+            'ref_no': 'ocr_python_123',
+        }, files={"file": file_object})
+        
+        data = json.loads(r.text)
+
+        if 'receipts' in data and len(data['receipts']) > 0 and 'items' in data['receipts'][0]:
+            items = data['receipts'][0]['items']
+            for item in items:
+                item_list.append(item['description'])
+
+    print(item_list)
+    return {"item_list": item_list}
